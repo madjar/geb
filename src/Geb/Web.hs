83a70fafb80542b178b47b816bf761229df774be
Georges Dubus <georges.dubus@compiletoi.net>
2015-08-04 17:48:19 +0200

    Some nice rendering of the raws for the front page

--- src/Geb/Web.hs
+++ src/Geb/Web.hs
@@ -7,8 +7,10 @@ import           Geb.Store.Raw
 import           Control.Monad.IO.Class
 import           Data.Monoid
 import qualified Data.Text              as T
+import Data.Text.Lazy (toStrict)
 import           Web.Spock.Safe
-
+import Lucid
+import Control.Applicative
 
 runGeb :: IO ()
 runGeb =
@@ -16,7 +18,15 @@ runGeb =
      runSpock 3000 $ spockT id $ website store
 
 website :: FileStore -> SpockT IO ()
-website store = do get root $ do r <- liftIO (raws store)
-                                 (text . T.pack . show) r
+website store = do get root $ index store
                    get ("hello" <//> var) $ \name ->
                        text ("Hello " <> name <> "!")
+
+
+index :: MonadIO m => FileStore -> ActionT m b
+index store = do allRaws <- liftIO (raws store)
+                 render $ ul_ $ mapM (li_ . toHtml . show) allRaws
+
+
+render :: MonadIO m => Html a -> ActionT m b
+render = html . toStrict . renderText . doctypehtml_ . body_
